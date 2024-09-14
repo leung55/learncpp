ed847a00de0159b9e64b53fb7750faf7e705a9c8
Edmund <20leunge@gmail.com>
2024-09-14 00:08:06 -0400

    Add potions to monster game

--- ch24quizzes/monstergame.cpp
+++ ch24quizzes/monstergame.cpp
@@ -38,12 +38,16 @@ public:
     Player(std::string_view name, char symbol='@', int health=10, int attackDamage=1, int gold=0, int pLevel=1)
     : Creature{name, symbol, health, attackDamage, gold}
     , pLevel{pLevel} {
+        // std::cout << "Player created.\n";
         std::cout << "Welcome, " << name << ".\n";
         std::cout << "You have " << health << " health and are carrying " << gold << " gold.\n";
 
     }
+    // ~Player() {std::cout << "player destroyed\n";}
 
     void levelUp() {pLevel += 1; attackDamage += 1;}
+    void addHealth(int new_health) {health += new_health;}
+    void addStrength(int new_strength) {attackDamage += new_strength;}
     int getLevel() const {return pLevel;}
     bool hasWon() const {return pLevel >= 20;}
 };
@@ -66,8 +70,8 @@ private:
 public:
     Monster(std::string_view name, char symbol, int health, int attackDamage, int gold)
     : Creature{name, symbol, health, attackDamage, gold} {}
-    Monster(Type mType) : Creature{monsterData[mType]} {}
-
+    Monster(Type mType) : Creature{monsterData[mType]} {/* std::cout << getName() << "monster created\n"; */}
+    // ~Monster() {std::cout << getName() << " destroyed\n";}
     static Type getRandomMonster() {return static_cast<Type>(Random::get(0,maxTypes-1));}
 };
 
@@ -110,6 +114,54 @@ public:
         }
         return escaped;
     }
+    void offerPotion() {
+        char userDrink;
+        std::cout << "You found a mythical potion! Do you want to drink it? [y/n]: ";
+        while(true) {
+            std::cin >> userDrink;
+            if(InputHandling::clearFailedExtraction() || userDrink != 'y' && userDrink != 'n') {
+                std::cout << "Invalid choice. Try again [y/n]: ";
+                continue;
+            }
+            InputHandling::ignoreLine();
+            break;
+        }
+        if(userDrink == 'y') {
+            int type{Random::get(1,3)};
+            int effect{Random::get(1,3)};
+
+            switch(type) {
+                case 1 : //health
+                    switch(effect) {
+                    case 1:
+                    case 2:
+                        p->addHealth(2); break;
+                    case 3:
+                        p->addHealth(5); break;
+                    }
+                    std::cout << "You drank a potion of health! You now have " << p->getHealth() << " health.\n";
+                    break;
+                case 2 : //strength
+                    switch(effect) {
+                        case 1:
+                        case 2:
+                        case 3:
+                            p->addStrength(1); break;
+                    }
+                    std::cout << "You drank a potion of strength! You now have " << p->getAttackDamage() << " attack damage.\n";
+                    break;
+                case 3: //poison
+                    switch(effect) {
+                        case 1:
+                        case 2:
+                        case 3:
+                            p->reduceHealth(1); break;
+                    }
+                    std::cout << "You drank a potion of poison! You now have " << p->getHealth() << " health.\n";
+                    break;
+            }
+        }
+    }
     void playGame() {
         std::string name;
         std::cout << "Enter your name: ";
@@ -134,6 +186,8 @@ public:
                 p->addGold(m->getGold());
                 p->levelUp();
                 std::cout << "You now have " << p->getGold() << " gold and are now at level " << p->getLevel() << " with " << p->getHealth() << " health.\n";
+                if(Random::get(1,10) <= 3)
+                    offerPotion();
                 continue;
             }
         }
@@ -149,19 +203,19 @@ public:
 
 int main()
 {
-	Creature o{ "orc", 'o', 4, 2, 10 };
-	o.addGold(5);
-	o.reduceHealth(1);
-	std::cout << "The " << o.getName() << " has " << o.getHealth() << " health and is carrying " << o.getGold() << " gold.\n";
+	// Creature o{ "orc", 'o', 4, 2, 10 };
+	// o.addGold(5);
+	// o.reduceHealth(1);
+	// std::cout << "The " << o.getName() << " has " << o.getHealth() << " health and is carrying " << o.getGold() << " gold.\n";
 
     
-    Monster m{ Monster::Type::dragon };
-	std::cout << "A " << m.getName() << " (" << m.getSymbol() << ") was created.\n";
-    for (int i{ 0 }; i < 10; ++i)
-	{
-		Monster m2{ Monster::getRandomMonster() };
-		std::cout << "A " << m2.getName() << " (" << m2.getSymbol() << ") was created.\n";
-	}
+    // Monster m{ Monster::Type::dragon };
+	// std::cout << "A " << m.getName() << " (" << m.getSymbol() << ") was created.\n";
+    // for (int i{ 0 }; i < 10; ++i)
+	// {
+	// 	Monster m2{ Monster::getRandomMonster() };
+	// 	std::cout << "A " << m2.getName() << " (" << m2.getSymbol() << ") was created.\n";
+	// }
     MonsterGame mg{};
     mg.playGame();
 	return 0;
